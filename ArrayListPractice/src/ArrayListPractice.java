408615326188957f419c92545dc23ab3329ac213
elaitha <emmalkeenan3@gmail.com>
2019-02-11 11:22:55 -0500

    Add start stuff

--- ArrayListPractice/src/ArrayListPractice.java
+++ ArrayListPractice/src/ArrayListPractice.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
 
 public class ArrayListPractice {
 
 	public static void main(String[] args) {
 		
+		ArrayList <Integer> myList = new ArrayList<Integer>();
+		myList.add(1);
+		myList.add(15);
+		
+			System.out.println(myList);
 		
 	}
 
