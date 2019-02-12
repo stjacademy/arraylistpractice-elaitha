a4569b6025ca3e51f8f777ecd6f4ef73008b6b04
elaitha <emmalkeenan3@gmail.com>
2019-02-12 10:32:36 -0500

    Use ArrayList to create prime factorizer

--- ArrayListPractice/src/ArrayListPractice.java
+++ ArrayListPractice/src/ArrayListPractice.java
@@ -3,12 +3,31 @@ import java.util.ArrayList;
 public class ArrayListPractice {
 
 	public static void main(String[] args) {
+		int num = 158;
 		
-		ArrayList <Integer> myList = new ArrayList<Integer>();
-		myList.add(1);
-		myList.add(15);
+		ArrayList <Integer> primes = new ArrayList<Integer>();
+		primes.add(2);
+		for(int i = 3; i <= num/2; i+=2) {
+			for(Integer prime : primes) {
+				if(i%prime!=0) {
+					primes.add(i);
+				}
+				break;
+			}
+		}
 		
-			System.out.println(myList);
+		ArrayList <Integer> primeFactors = new ArrayList<Integer>();
+		
+		for(int i = 0; i < primes.size(); i++) {
+			if(num%primes.get(i)==0) {
+				primeFactors.add(primes.get(i));
+				num/=primes.get(i);
+				i--;
+			}
+		}
+		
+		System.out.println(primeFactors);
+
 		
 	}
 
